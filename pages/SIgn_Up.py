f8eabb7000928ba8b8c41c33869f9aef93e571b9
mosesard14 <moses.putra@ti.ukdw.ac.id>
2025-11-18 09:41:14 +0700

    Update SIgn_Up.py

--- pages/SIgn_Up.py
+++ pages/SIgn_Up.py
@@ -5,5 +5,5 @@ username_input = st.text_input('Buat Username ')
 password_input = st.text_input('Password Baru', type= 'password')
 confirm_password = st.text_input('Konfirmasi Password', type= 'password')
 
-if st.button('Sign Up'):
+if st.button('Mendaftar'):
     st.switch_page('pages/Login.py')
