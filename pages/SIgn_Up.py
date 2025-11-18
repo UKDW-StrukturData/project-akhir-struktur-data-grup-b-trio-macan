bb5b805c4f004af6df179958beb2c8b8c734b1c8
mosesard14 <moses.putra@ti.ukdw.ac.id>
2025-11-18 09:27:44 +0700

    perubahan 0.5

--- pages/SIgn_Up.py
+++ pages/SIgn_Up.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
+email_input = st.text_input('Masukkan Email anda')
 username_input = st.text_input('Buat Nama Pengguna ')
 password_input = st.text_input('Kata Sandi Baru', type= 'password')
 confirm_password = st.text_input('Konfirmasi Kata Sandi', type= 'password')
+
 if st.button('Sign Up'):
     st.switch_page('pages/Login.py')
