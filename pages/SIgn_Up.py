1fbcf360ac8c49457f8f26f15c1214230f7a345b
mosesard14 <moses.putra@ti.ukdw.ac.id>
2025-11-18 09:28:57 +0700

    Update SIgn_Up.py

--- pages/SIgn_Up.py
+++ pages/SIgn_Up.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 email_input = st.text_input('Masukkan Email anda')
 username_input = st.text_input('Buat Nama Pengguna ')
 password_input = st.text_input('Kata Sandi Baru', type= 'password')
 confirm_password = st.text_input('Konfirmasi Kata Sandi', type= 'password')
 
 =======
+=======
+>>>>>>> Stashed changes
 
 email_input = st.text_input('Masukkan Email anda')
 username_input = st.text_input('Buat Username ')
