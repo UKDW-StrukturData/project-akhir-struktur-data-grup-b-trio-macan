731132a1e3a5470337db7e1388d26356cf1d642c
mosesard14 <moses.putra@ti.ukdw.ac.id>
2025-12-02 10:46:25 +0700

    menamppilkan wilayah berdasarkan adm

--- pages/Home.py
+++ pages/Home.py
@@ -44,7 +44,10 @@ kondisi = "-"
 if wilayah_pilihan:
     adm4 = df_kode[df_kode["nama"] == wilayah_pilihan]["kode"].values[0]
     url = f"https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4={adm4}"
-    st.success(f'Berhasil menampilkan untuk wilayah {wilayah_pilihan}')
+    if url in wilayah_pilihan: 
+        st.success(f'Berhasil menampilkan untuk wilayah {wilayah_pilihan}')
+    else:
+        st.error('Masukkan wilayah yang spesifik')
     try:
         r = requests.get(url, timeout=15)
         r.raise_for_status()
